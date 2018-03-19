864151f29baba4b6bbf20f76162db97e610dc7f7
Grigori Fursin <Grigori.Fursin@cTuning.org>
2018-03-19 14:29:18 +0100

    artifact description dummy

--- script/mobilenets-armcl-opencl/benchmark-add-dimensions.py
+++ script/mobilenets-armcl-opencl/benchmark-add-dimensions.py
@@ -7,8 +7,7 @@ def do(i):
     # List performance entries
     r=ck.access({'action':'search',
                  'module_uoa':'experiment',
-                 'data_uoa':'mobilenets-performance-*',
-                 'repo_uoa':'local'})
+                 'data_uoa':'mobilenets-performance-*'})
     if r['return']>0: return r
     lst=r['lst']
 
@@ -33,6 +32,20 @@ def do(i):
 
         # Updating meta if needed
         dd['meta']['scenario_module_uoa']='a555738be4b65860' # module:request.asplos18
+
+        dd['meta']['model_species']='07d4e7aa3750ddc6' # model.species:mobilenets
+
+        dd['meta']['dataset_species']='ImageNet' # dataset species (free format)
+        dd['meta']['dataset_size']=500 # number of images ...
+
+        dd['meta']['platform_species']='embedded' # embedded vs server (maybe other classifications such as edge)
+
+        dd['meta']['platform_peak_power']=4.5 #Watts
+
+        dd['meta']['platform_price']=239 # $
+        dd['meta']['platform_price_date']='20170425' # date
+
+        dd['meta']['artifact']='' # date
 
         # Updating entry
         r=ck.access({'action':'update',
@@ -59,6 +72,16 @@ def do(i):
                if r['return']>0: return r
                d=r['dict']
 
+               mult=d.get('##choices#env#CK_ENV_MOBILENET_WIDTH_MULTIPLIER#min','')
+
+               if mult==0.25: size=1990786
+               elif mult==0.5: size=5459810
+               elif mult=0.75: size=10498594
+               elif mult=1.0: size=17106694
+               else:
+                  return {'return':1, 'error':'unknown width multiplier "'+str(mult)+'"'}
+
+               d['##features#model_size#min']=size
 
                # Save updated dict
                r=ck.save_json_to_file({'json_file':p1, 'dict':d, 'sort_keys':'yes'})
