e9beb61d049037a0c96f9f2392fe9df43d3c505c
Grigori Fursin <Grigori.Fursin@cTuning.org>
2018-03-20 11:44:37 +0100

    added 3 dimensions (CPU freq, GPU freq, precision)

--- script/mobilenets-armcl-opencl/benchmark-add-dimensions.py
+++ script/mobilenets-armcl-opencl/benchmark-add-dimensions.py
@@ -7,6 +7,7 @@ def do(i):
     # List performance entries
     r=ck.access({'action':'search',
                  'module_uoa':'experiment',
+#                 'repo_uoa':'ck-request-asplos18-results'})
                  'data_uoa':'mobilenets-performance-*'})
     if r['return']>0: return r
     lst=r['lst']
@@ -46,6 +47,8 @@ def do(i):
 
         dd['meta']['artifact']='08da9685582866a0' # artifact description
 
+        dd['meta']['model_precision']='fp32'
+
         # Updating entry
         r=ck.access({'action':'update',
                      'module_uoa':'experiment',
@@ -59,9 +62,13 @@ def do(i):
         if r['return']>0: return r
 
         # Checking points to aggregate
+        os.chdir(path)
         dperf=os.listdir(path)
         for f in dperf:
-            if f.endswith('.flat.json'):
+            if f.endswith('.cache.json'):
+               os.system('git rm -f '+f)
+
+            elif f.endswith('.flat.json'):
                ck.out(' * '+f)
 
                # Load performance file 
@@ -81,6 +88,9 @@ def do(i):
                   return {'return':1, 'error':'unknown width multiplier "'+str(mult)+'"'}
 
                d['##features#model_size#min']=size
+
+               d['##features#cpu_freq#min']='807'
+               d['##features#gpu_freq#min']=''
 
                # Save updated dict
                r=ck.save_json_to_file({'json_file':p1, 'dict':d, 'sort_keys':'yes'})
